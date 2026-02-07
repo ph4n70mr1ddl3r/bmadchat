22a446ea8dac60bddb2c78f72c5d22da52356c10
ph4n70mr1ddl3r <105593742+ph4n70mr1ddl3r@users.noreply.github.com>
2026-02-07 09:37:13 +0800

    fix: add package metadata and clean up ESLint disable comments

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -42,7 +42,6 @@ describe('sum', () => {
         expect(sum(Number.MAX_SAFE_INTEGER, -1)).toBe(Number.MAX_SAFE_INTEGER - 1);
     });
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     test('throws TypeError for non-number arguments', () => {
         expect(() => sum('1' as any, 2)).toThrow(TypeError); // eslint-disable-line @typescript-eslint/no-explicit-any
         expect(() => sum(1, '2' as any)).toThrow(TypeError); // eslint-disable-line @typescript-eslint/no-explicit-any
