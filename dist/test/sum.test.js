c43f5b5251ad6cfae9b50d0d4b291d193eeaa19a
ph4n70mr1ddl3r <105593742+ph4n70mr1ddl3r@users.noreply.github.com>
2026-02-07 08:31:51 +0800

    Remove tracked build artifacts from git
    - Remove dist/ directory from git tracking (build outputs should be ignored)
    - Remove coverage/ directory from git tracking (test reports should be ignored)
    - Remove junit.xml from git tracking (generated test report)
    
    These files are now properly excluded by .gitignore and will not be committed in future.

--- dist/test/sum.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../src/index");
-describe('sum', () => {
-    test('adds two numbers', () => {
-        expect((0, index_1.sum)(1, 2)).toBe(3);
-    });
-});
